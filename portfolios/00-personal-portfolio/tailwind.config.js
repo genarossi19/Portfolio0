6e37bde418837620c5a2a6303c557f6e75e5de41
Genaro Rossi <genaros.gr@gmail.com>
2024-03-05 14:26:37 -0300

    implementando font poppins

--- portfolios/00-personal-portfolio/tailwind.config.js
+++ portfolios/00-personal-portfolio/tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+      },
+      colors: {
+        backgray: "#141414",
+        lightgray: "#282828",
+        light: "#FFFFFC",
+        darkpurple: "#230046",
+        lightpurple: "#320064",
+      }
+      
+    },
   },
   plugins: [],
-}
-
+};
