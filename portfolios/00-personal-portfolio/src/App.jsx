f7c684eaae14fcd9cc62081c7579e8449be51012
Genaro Rossi <genaros.gr@gmail.com>
2024-03-06 13:32:20 -0300

    Texto y animaciones

--- portfolios/00-personal-portfolio/src/App.jsx
+++ portfolios/00-personal-portfolio/src/App.jsx
@@ -1,21 +1,85 @@
 import "/src/index.css";
 import Navbar from "./components/Navbar 01/Navbar";
 import phoneme from "./assets/phoneme.png";
+import { motion } from "framer-motion";
 
 function App() {
+  const textVariants = {
+    hidden: { opacity: 0, x: -20 },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: { delay: 0.2, staggerChildren: 0.1 },
+    },
+  };
+
+  const textVariants2 = {
+    hidden: { opacity: 0, x: -20 },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: { delay: 0.1, staggerChildren: 0.025 },
+    },
+  };
+
+  const letterVariants = {
+    hidden: { opacity: 0, x: -20 },
+    visible: { opacity: 1, x: 0 },
+  };
+
+  const title = "Genaro Rossi";
+  const description = "Desarrollador Junior frontend y estudiante de Ingeniería en Sistemas."
+
   return (
+    // Pantalla principal
     <div id="screen" className="bg-backgray min-h-screen  ">
       <Navbar />
-      <div className="flex flex-row">
-        <div id="phone" className=" h-auto max-h-[800px]  p-8 ">
-          <img
+      {/* div pagina */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-12 min-h-screen ">
+        {/* div Imagen */}
+        <div id="phone" className=" relative h-screen  p-8 ms-2 lg:col-span-4 ">
+          {/* Imagen */}
+          <motion.img
             id="phoneimage"
             src={phoneme}
             alt="phone"
-            className="max-h-[800px]  phoneimage"
+            initial={{ opacity: 0, y: 50 }} // Posición inicial en la parte inferior
+            animate={{ opacity: 1, y: 0 }} // Posición final en el centro
+            transition={{ duration: 1 }}
+            className="h-1/2 sm:h-full object-cover  phoneimage "
           />
         </div>
-        <h1 className="text-8xl text-purple-500 font-poppins font-bold ">Genaro Rossi</h1>
+        {/* div content */}
+        <div id="content" className="col-span-8">
+          {/* div Texto */}
+          <div id="text" className="inline-block">
+            {/* Texto */}
+            <motion.h1
+              className="text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl text-purple-500 font-poppins font-bold textshadow text-center m-8 lg:ms-8"
+              variants={textVariants}
+              initial="hidden"
+              animate="visible"
+            >
+              {title.split("").map((char, index) => (
+                <motion.span key={index} variants={letterVariants}>
+                  {char}
+                </motion.span>
+              ))}
+            </motion.h1>
+            <motion.p
+              className="text-white text-center text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl"
+              variants={textVariants2}
+              initial="hidden"
+              animate="visible"
+            >
+              {description.split("").map((char, index) => (
+                <motion.span key={index} variants={letterVariants}>
+                  {char}
+                </motion.span>
+              ))}
+            </motion.p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- portfolios/00-personal-portfolio/src/components/Navbar 01/NavbarDefault.tsx
+++ portfolios/00-personal-portfolio/src/components/Navbar 01/NavbarDefault.tsx
@@ -1,41 +1,45 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
 
 function NavbarDefault() {
-    return (
-      <nav className="flex justify-between items-center px-10 py-5">
-        <div>
-          <h2 className="text-xl text-white font-semibold">genarossi19</h2>
-        </div>
-  
-        <ul className="flex items-center gap-2 text-[#bdbdc0] font-medium font-poppins">
-          <li>
-            <a href="" className="p-3 hover:text-white">
-              Inicio
-            </a>
-          </li>
-          <li>
-            <a href="" className="p-3 hover:text-white">
-              Sobre mi
-            </a>
-          </li>
-          <li>
-            <a href="" className="p-3 hover:text-white">
-              Portfolio
-            </a>
-          </li>
-          <li>
-            <a href="" className="p-3 hover:text-white">
-              Contacto
-            </a>
-          </li>
-        </ul>
-  
-        <div>
-          <button className="font-poppins text-sm px-4 py-2 rounded-full text-white font-medium bg-lightpurple  border border-white border-opacity-[.08] hover:border-opacity-25">
-            Toast
-          </button>
-        </div>
-      </nav>
-    );
-  }
-  export default NavbarDefault;
+  return (
+    <nav className=" justify-between items-center px-10 py-5 hidden sm:flex">
+      <div className="flex flex-row items-center gap-4">
+        <FaGithub className="text-white w-10 h-10" />
+        <a href="https://github.com/genarossi19" className="no-underline"target="_blank" rel="noopener noreferrer" >
+          <h2 className="text-xl text-white font-semibold hover:border-b hover:border-purple-600 hover:text-purple-500 textshadow transition-all duration-300 ease-in-out ">genarossi19</h2>
+        </a>
+      </div>
+
+      <ul className="flex items-center gap-2 text-[#bdbdc0] font-medium font-poppins">
+        <li>
+          <a href="" className="p-3 hover:text-white">
+            Inicio
+          </a>
+        </li>
+        <li>
+          <a href="" className="p-3 hover:text-white">
+            Sobre mi
+          </a>
+        </li>
+        <li>
+          <a href="" className="p-3 hover:text-white">
+            Portfolio
+          </a>
+        </li>
+        <li>
+          <a href="" className="p-3 hover:text-white">
+            Contacto
+          </a>
+        </li>
+      </ul>
+
+      <div>
+        <button className="font-poppins text-sm px-4 py-2 rounded-full text-white font-medium bg-lightpurple  border border-white border-opacity-[.08] hover:border-opacity-25">
+          Toast
+        </button>
+      </div>
+    </nav>
+  );
+}
+export default NavbarDefault;
