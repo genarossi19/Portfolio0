12acc1ff9b1076669af283fcac1b0e76224842bd
genarossi19 <genaros.gr@gmail.com>
2024-03-14 18:58:32 -0300

    prueba de divs

--- portfolios/01-mobile-portfolio/src/App.jsx
+++ portfolios/01-mobile-portfolio/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div id="screen" className="relative bg-red-200 min-h-screen">
+      <nav className="bg-gray-600 w-full h-[30%] absolute top-0 left-0 z-10"></nav>
+      <main className="bg-green-200 h-screen relative">
+        <div className="bg-yellow-200 min-h-full">
+          {/* Contenido del div amarillo */}
+        </div>
+        <div className="bg-blue-200 h-[300px]">
+          {/* Otro contenido */}
+        </div>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
